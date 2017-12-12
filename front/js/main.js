d9dbc2626de79c1ae1a3ffee7efaaf3ba09ba6be
judith435 <judithguttmann@gmail.com>
2017-12-12 03:44:07 +0200

    test

--- front/js/main.js
+++ front/js/main.js
@@ -5,9 +5,9 @@
 var app = {
     debugMode: false,   
     schoolApi:        "http://localhost/project-3-john-bryce/back/api/api.php",
-    adminImagePath:   "http://localhost/project-3-john-bryce/back/images/admins/image_for_admin_id_",
-    courseImagePath:  "http://localhost/project-3-john-bryce/back/images/courses/image_for_course_id_",
-    studentImagePath: "http://localhost/project-3-john-bryce/back/images/students/image_for_student_id_"
+    adminImagePath:   "http://localhost/project-3-john-bryce/images/admins/image_for_admin_id_",
+    courseImagePath:  "http://localhost/project-3-john-bryce/images/courses/image_for_course_id_",
+    studentImagePath: "http://localhost/project-3-john-bryce/images/students/image_for_student_id_"
 };
 
 jQuery(document).ready(function() {
